7edb973af60dad5b3441bbf20f9f884f070fbe6a
Ronan Kelly <ronankelly@unknown.home>
2026-04-27 15:14:42 +0100

    Add day 4 challenge

---
+++ day4challenge.js
@@ -0,0 +1,10 @@
+let num = 9;
+
+if (num % 2 === 0) { //modulus '%' checks divisibility, '===' checks if theres a remainder of zero
+    console.log("Number is divisible by 2");
+} else {
+    console.log("Number is not divisible by 2");
+}
+
+
+//in this instance 9 is not divisible by 2 with a remainder of 0 (1)
