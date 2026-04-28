fde51e0a27d0ee374557dc392b5166bcc5fa36cf
Ronan Kelly <ronankelly@unknown.home>
2026-04-28 19:34:48 +0100

    Day 5: functions

---
+++ day5.js
@@ -0,0 +1,30 @@
+function greet(name) {          //I am creating a function which creates more repeatable code.
+    return "Hello " + name + ", welcome!";     
+}                               //Instead of asking the console to output "Hello Ronan", I am
+console.log(greet("Ronan"));    //getting it to write the logic once, then reuse it
+console.log(greet("Laura"));   
+
+//function --> "I'm creating a function"
+//greet --> name of the function
+//(name) --> input (parameter)
+// ^Think: "I'm creating a machine called greet that takes a name"
+
+//return (IMPORTANT): This means "send this value back",
+//without the return the function gives nothing. 
+
+function add(a, b) { //this function receives numbers and adds them, returning them to the console.
+    return a + b;
+}
+
+console.log(add(5, 3)); //this sends the numbers to the function and prints the results
+
+function checkCals (cals, target) { //this function checks if calories > target, then returns bool result to console
+    if (cals > target) {
+        return cals - target + " Over target";
+    } else {
+        return target - cals + " Under target";
+    }
+}
+
+console.log(checkCals(2000, 2200));
+
