1cd41d3764488154cf505f8eff780859e1306c55
Ronan Kelly <ronankelly@Ronans-MacBook-Pro.local>
2026-04-30 17:12:26 +0100

    Day 7: Challenge complete

--- day7challenge.js
+++ day7challenge.js
@@ -1,8 +1,10 @@
 let calsBurned = [200, 350, 150, 400, 180];
 let hiBurnCounter = 0;
+let totalCals = 0;
 
 function analyseWorkouts(calsBurned) {
     for (let i = 0; i < calsBurned.length; i++) {
+        totalCals = totalCals + calsBurned[i];
         let current = calsBurned[i];
         if (current > 300) {
             hiBurnCounter++;
@@ -15,7 +17,11 @@ function analyseWorkouts(calsBurned) {
 }
 
 analyseWorkouts(calsBurned);
+console.log(totalCals + " total calories burned on the workouts.")
 console.log(hiBurnCounter + " total high-cal burning workouts.")
+
 
 //Above I created an array, function, loop and if/else statement to determine if
 //the array values are either high-calorie burning, or low-calorie burning.
+//This program also counts the number of workouts that were high-calorie burning &
+//the total number of calories burned, and returns them in the console.
