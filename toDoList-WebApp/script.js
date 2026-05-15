6c3da843584fff7fcb29b7f29520e43bcc02b4d9
Ronan Kelly <ronankelly@Ronans-MacBook-Pro.local>
2026-05-15 03:08:14 +0100

    Day 15: Static -> Interactive website!

--- toDoList-WebApp/script.js
+++ toDoList-WebApp/script.js
@@ -1,8 +1,13 @@
 console.log("JS Connected");
 
 let button = document.getElementById("submitButton");
+let input = document.getElementById("taskInput");
+let taskList = document.getElementById("taskList");
 
 button.addEventListener("click", function() {
+    let newTask = document.createElement("li");
+    newTask.textContent = input.value;
+    taskList.appendChild(newTask);
     console.log("Button clicked");
+    console.log(input.value);
 });
-
